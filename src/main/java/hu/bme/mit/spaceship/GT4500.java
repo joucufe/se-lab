456ea2e99c67669bad9dd5378374247e27b55478
unknown <168750@bme.hu>
2023-10-11 08:33:01 +0200

    modified:   src/main/java/hu/bme/mit/spaceship/GT4500.java

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,7 +79,7 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
-        firingSuccess=fireTorpedo(ALL);
+        firingSuccess=true;
         break;
     }
     //branch-B módosítás
